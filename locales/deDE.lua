633a00f87811180b58450417e13dcbb63b0f5b36
Areko <77594959+WoW-Areko@users.noreply.github.com>
2021-04-10 21:20:00 +0200

    feat: add locale

--- locales/deDE.lua
+++ locales/deDE.lua
@@ -1,4 +1,13 @@
 local ADDONNAME = ...
 
+
+
+
+
 local L = LibStub("AceLocale-3.0"):NewLocale(ADDONNAME, "deDE") -- luacheck: ignore 113/LibStub
 if not L then return end
+
+L.CATALYSTS_D = "%d |4Katalysator:Katalysatoren"
+L.CATALYSTS_M = "? Katalysatoren"
+L.COVENANT_REWARDS_DD = "Pakt Belohnungen |cFFFFFFFF%d/%d|r"
+L.SHARED_REWARDS_DD = "Geteilte Belohnungen |cFFFFFFFF%d/%d|r"
--- locales/enUS.lua
+++ locales/enUS.lua
@@ -6,3 +6,8 @@ debug = true
 
 local L = LibStub("AceLocale-3.0"):NewLocale(ADDONNAME, "enUS", true, debug) -- luacheck: ignore 113/LibStub
 if not L then return end
+
+L.CATALYSTS_D = "%d |4Catalyst:Catalysts"
+L.CATALYSTS_M = "? Catalysts"
+L.COVENANT_REWARDS_DD = "Covenant rewards |cFFFFFFFF%d/%d|r"
+L.SHARED_REWARDS_DD = "Shared rewards |cFFFFFFFF%d/%d|r"
