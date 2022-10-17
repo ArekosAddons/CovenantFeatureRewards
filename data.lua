59ffe24c98697cf5978bce48e6bb2bbedbd3047d
Areko <77594959+WoW-Areko@users.noreply.github.com>
2022-10-17 22:35:29 +0200

    feat: move item data to own file

--- data.lua
+++ data.lua
@@ -13,32 +13,6 @@ local REWARD_TYPE_QUEST     = CFR.REWARD_TYPES.QUEST
 local REWARD_TYPE_TOY       = CFR.REWARD_TYPES.TOY
 
 
--- Tooltip items
-CFR.kyrian_items = {
-    -- [itemID] = covenant theme,
-}
-CFR.necrolord_items = {
-    -- [itemID] = covenant theme,
-}
-CFR.nightfae_items = {
-    -- [itemID] = covenant theme,
-    [177698] = NIGHTFAE,    -- Spirit (Uncommon)
-    [177699] = NIGHTFAE,    -- Great Spirit (Rare)
-    [177700] = NIGHTFAE,    -- Divine Spirit (Epic)
-    [178874] = NECROLORD,   -- Spirit (Uncommon)
-    [178877] = NECROLORD,   -- Great Spirit (Uncommon)
-    [178878] = NECROLORD,   -- Divine Spirit (Epic)
-    [178879] = KYRIAN,      -- Divine Spirit (Epic)
-    [178880] = KYRIAN,      -- Great Spirit (Rare)
-    [178881] = KYRIAN,      -- Spirit (Uncommon)
-    [178882] = VENTHYR,     -- Spirit (Uncommon)
-    [178883] = VENTHYR,     -- Great Spirit (Rare)
-    [178884] = VENTHYR,     -- Divine Spirit (Epic)
-}
-CFR.venthyr_items = {
-    -- [itemID] = covenant theme,
-}
-
 
 -- LOOKUP: speciesID: https://wowpedia.fandom.com/wiki/BattlePetSpeciesID
 
---
+++ data/items.lua
@@ -0,0 +1,33 @@
+local ADDONNAME, CFR = ...
+
+
+local KYRIAN    = CFR.COVENANTS.KYRIAN
+local NECROLORD = CFR.COVENANTS.NECROLORD
+local NIGHTFAE  = CFR.COVENANTS.NIGHTFAE
+local VENTHYR   = CFR.COVENANTS.VENTHYR
+
+-- Tooltip items
+CFR.kyrian_items = {
+    -- [itemID] = covenant theme,
+}
+CFR.necrolord_items = {
+    -- [itemID] = covenant theme,
+}
+CFR.nightfae_items = {
+    -- [itemID] = covenant theme,
+    [177698] = NIGHTFAE,    -- Spirit (Uncommon)
+    [177699] = NIGHTFAE,    -- Great Spirit (Rare)
+    [177700] = NIGHTFAE,    -- Divine Spirit (Epic)
+    [178874] = NECROLORD,   -- Spirit (Uncommon)
+    [178877] = NECROLORD,   -- Great Spirit (Uncommon)
+    [178878] = NECROLORD,   -- Divine Spirit (Epic)
+    [178879] = KYRIAN,      -- Divine Spirit (Epic)
+    [178880] = KYRIAN,      -- Great Spirit (Rare)
+    [178881] = KYRIAN,      -- Spirit (Uncommon)
+    [178882] = VENTHYR,     -- Spirit (Uncommon)
+    [178883] = VENTHYR,     -- Great Spirit (Rare)
+    [178884] = VENTHYR,     -- Divine Spirit (Epic)
+}
+CFR.venthyr_items = {
+    -- [itemID] = covenant theme,
+}
