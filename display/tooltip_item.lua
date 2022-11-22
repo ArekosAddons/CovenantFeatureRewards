ee6f6d8a68f8597e8b794cd2e90a39cebc45b272
Areko <77594959+WoW-Areko@users.noreply.github.com>
2022-11-22 15:06:53 +0100

    feat: use new TooltipDataProcessor system

--- display/tooltip_item.lua
+++ display/tooltip_item.lua
@@ -1,29 +1,17 @@
 local ADDONNAME, CFR = ...
 
+local supportedTooltips = {
+    [GameTooltip] = true,
+    [ItemRefTooltip] = true,
+}
 
-local function hook(tooltip)
-    local _, link = tooltip:GetItem()
-    if not link then return end
+TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, function(tooltip, data)
+    if not supportedTooltips[tooltip] then return end
 
-    local id = tonumber( string.match(link, "item:(%d+)") )
-    if not id then return end
-
-    if not CFR:HasItemReward(id) then return end
+    local id = data.id
+    if not id or not CFR:HasItemReward(id) then return end
 
     local rewards = CFR:GetRewardsForItem(id)
     CFR:GameTooltip_AddRewards(tooltip, rewards)
     tooltip:Show()
-end
-
-local function hook_tooltip(tooltip)
-    hooksecurefunc(tooltip, "SetBagItem", hook)
-    hooksecurefunc(tooltip, "SetHyperlink", hook)
-    hooksecurefunc(tooltip, "SetInventoryItem", hook)
-    hooksecurefunc(tooltip, "SetInventoryItemByID", hook)
-    hooksecurefunc(tooltip, "SetItemByID", hook)
-    hooksecurefunc(tooltip, "SetQuestItem", hook)
-    hooksecurefunc(tooltip, "SetQuestLogItem", hook)
-end
-
-hook_tooltip(GameTooltip)
-hook_tooltip(ItemRefTooltip)
+end)
